38c637a45f922d8de853664b392943bb83a2f406
Alejandrr <omelkoomelko1@gmail.com>
2023-01-21 05:39:57 +0200

    Completed

--- Restoran/Program.cs
+++ Restoran/Program.cs
@@ -2,6 +2,11 @@
 using Microsoft.Extensions.Configuration;
 using System.Linq;
 using Restoran3;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore.Storage;
+using Castle.Core.Resource;
+using System.Data;
+
 var builder = new ConfigurationBuilder();
 builder.SetBasePath(Directory.GetCurrentDirectory());
 builder.AddJsonFile("appsettings.json");
@@ -131,128 +136,157 @@ using (RestoranDbContext db = new RestoranDbContext(options))
     Console.WriteLine($"***********Max salary:{max}|Min salary:{min}|Count of workers :{count}");
 }
 
-//Lazy loading
+////Lazy loading
+//using (RestoranDbContext db = new RestoranDbContext(options))
+//{
+
+//    db.WorkRanks.Add(
+//        new WorkRank { WrFullName = "New lazy entity1" }
+//        );
+//    db.Workers.Add(
+//        new Worker
+//        {
+//            WIpn = "09870543",
+//            WDocument = "90923477",
+//            WPostId = 27,
+//            WSalary = 3221,
+//            WPib = "New lazy entity23"
+//        }
+//        );
+//    db.Workers.Add(
+//   new Worker
+//   {
+//       WIpn = "09871543",
+//       WDocument = "90121487",
+//       WPostId = 27,
+//       WSalary = 3271,
+//       WPib = "New lazy entity2we"
+//   }
+//   );
+//    db.Workers.Add(
+//   new Worker
+//   {
+//       WIpn = "09876243",
+//       WDocument = "90123387",
+//       WPostId = 27,
+//       WSalary = 3921,
+//       WPib = "New lazy entit3q"
+//   }
+//   );
+//    db.SaveChanges();
+//}
+//using (RestoranDbContext db = new RestoranDbContext(options))
+//{
+//    var workers = db.Workers.ToList();
+//    foreach (var val in workers)
+//        Console.WriteLine($"{val.WPib}-|-{val.WPost.WrFullName}");
+
+//}
+
+
+//////////////AS NO TRACKING
+//using (RestoranDbContext db = new RestoranDbContext(options))
+//{
+//    var customer = db.Customers.AsNoTracking().FirstOrDefault();
+//    if(customer != null)
+//    {
+//        customer.CPib = "No traking";
+//        db.SaveChanges();
+//    }
+
+//    var customers = db.Customers.ToList();
+//    foreach(var n in customers)
+//    {
+//        Console.WriteLine($"{n.CPib}");
+//    }
+//}
+//using (RestoranDbContext db = new RestoranDbContext(options))
+//{
+//    var c = db.Customers.FirstOrDefault();
+//    var c2 = db.Customers.AsNoTracking().FirstOrDefault();
+
+//    if (c != null && c2 != null)
+//    {
+//        Console.WriteLine($"Before Customer1 :{c.CPib}   Customer2 :{c2.CPib}");
+//        c.CPib = "Changed";
+//        Console.WriteLine($"After Customer1 :{c.CPib}   Customer2 :{c2.CPib}");
+
+//    }
+//}
+////////////////////Eager loading
+//using (RestoranDbContext db = new RestoranDbContext(options))
+//{
+
+//    db.WorkRanks.Add(
+//          new WorkRank { WrFullName = "New lazy entity1" }
+//    );
+//    db.Workers.Add(
+// new Worker
+// {
+//     WIpn = "098711624",
+//     WDocument = "90121187",
+//     WPostId = 28,
+//     WSalary = 3921,
+//     WPib = "New eager entity"
+// });
+//    db.SaveChanges();
+//    var wr = db.WorkRanks.Include(x => x.Workers).ToList();
+//    foreach(var worker in wr)
+//    {
+//        Console.WriteLine($"{worker.WrFullName}-|-{worker.Workers[0].WPib}");
+//    }
+//};
+////Explicit loading
+//using (RestoranDbContext db = new RestoranDbContext(options))
+//{
+//   db.Workers.Add(
+//        new Worker
+//        {
+//            WIpn = "098711624",
+//            WDocument = "90121187",
+//            WPostId = 1,
+//            WSalary = 3921,
+//            WPib = "New eager entity"
+//        });
+
+//    db.SaveChanges();
+
+//    WorkRank? wr = db.WorkRanks.FirstOrDefault();
+//    if(wr != null)
+//    {
+//        db.Entry(wr).Collection(x => x.Workers).Load();
+//        foreach(var val in wr.Workers)
+//        {
+//            Console.WriteLine($"{val.WPostId}|{val.WId}|{val.WPib}");
+//        }
+//    }
+//}
+
+//Procedures
 using (RestoranDbContext db = new RestoranDbContext(options))
 {
+    Console.WriteLine("******Proc");
 
-    db.WorkRanks.Add(
-        new WorkRank { WrFullName = "New lazy entity1" }
-        );
-    db.Workers.Add(
-        new Worker
-        {
-            WIpn = "09870543",
-            WDocument = "90923477",
-            WPostId = 27,
-            WSalary = 3221,
-            WPib = "New lazy entity23"
-        }
-        );
-    db.Workers.Add(
-   new Worker
-   {
-       WIpn = "09871543",
-       WDocument = "90121487",
-       WPostId = 27,
-       WSalary = 3271,
-       WPib = "New lazy entity2we"
-   }
-   );
-    db.Workers.Add(
-   new Worker
-   {
-       WIpn = "09876243",
-       WDocument = "90123387",
-       WPostId = 27,
-       WSalary = 3921,
-       WPib = "New lazy entit3q"
-   }
-   );
-    db.SaveChanges();
+    SqlParameter parameter = new()
+    {
+        ParameterName = "@name",
+        SqlDbType = System.Data.SqlDbType.VarChar,
+        Direction = System.Data.ParameterDirection.Input,
+        Size = 3000,
+        SqlValue="Added from EF labs"
+    };
+  //  db.Database.ExecuteSqlRaw("addClient @name", parameter) executed;
+
+    var customers = db.Customers;
+    foreach(Customer val in customers)
+         Console.WriteLine($"{val.CPib}");
 }
+//Functions
 using (RestoranDbContext db = new RestoranDbContext(options))
 {
-    var workers = db.Workers.ToList();
-    foreach (var val in workers)
-        Console.WriteLine($"{val.WPib}-|-{val.WPost.WrFullName}");
+    Console.WriteLine("******Func");
+    var ranks = db.WorkRanks.FromSqlRaw("SELECT * FROM GetWorkRankes()").ToList();
+    foreach (var val in ranks)
+        Console.WriteLine($"{val.WrFullName}");
 
-}
-
-
-////////////AS NO TRACKING
-using (RestoranDbContext db = new RestoranDbContext(options))
-{
-    var customer = db.Customers.AsNoTracking().FirstOrDefault();
-    if(customer != null)
-    {
-        customer.CPib = "No traking";
-        db.SaveChanges();
-    }
-
-    var customers = db.Customers.ToList();
-    foreach(var n in customers)
-    {
-        Console.WriteLine($"{n.CPib}");
-    }
-}
-using (RestoranDbContext db = new RestoranDbContext(options))
-{
-    var c = db.Customers.FirstOrDefault();
-    var c2 = db.Customers.AsNoTracking().FirstOrDefault();
-
-    if (c != null && c2 != null)
-    {
-        Console.WriteLine($"Before Customer1 :{c.CPib}   Customer2 :{c2.CPib}");
-        c.CPib = "Changed";
-        Console.WriteLine($"After Customer1 :{c.CPib}   Customer2 :{c2.CPib}");
-
-    }
-}
-//////////////////Eager loading
-using (RestoranDbContext db = new RestoranDbContext(options))
-{
- 
-    db.WorkRanks.Add(
-          new WorkRank { WrFullName = "New lazy entity1" }
-    );
-    db.Workers.Add(
- new Worker
- {
-     WIpn = "098711624",
-     WDocument = "90121187",
-     WPostId = 28,
-     WSalary = 3921,
-     WPib = "New eager entity"
- });
-    db.SaveChanges();
-    var wr = db.WorkRanks.Include(x => x.Workers).ToList();
-    foreach(var worker in wr)
-    {
-        Console.WriteLine($"{worker.WrFullName}-|-{worker.Workers[0].WPib}");
-    }
-};
-//Explicit loading
-using (RestoranDbContext db = new RestoranDbContext(options))
-{
-   db.Workers.Add(
-        new Worker
-        {
-            WIpn = "098711624",
-            WDocument = "90121187",
-            WPostId = 1,
-            WSalary = 3921,
-            WPib = "New eager entity"
-        });
-
-    db.SaveChanges();
-
-    WorkRank? wr = db.WorkRanks.FirstOrDefault();
-    if(wr != null)
-    {
-        db.Entry(wr).Collection(x => x.Workers).Load();
-        foreach(var val in wr.Workers)
-        {
-            Console.WriteLine($"{val.WPostId}|{val.WId}|{val.WPib}");
-        }
-    }
 }
